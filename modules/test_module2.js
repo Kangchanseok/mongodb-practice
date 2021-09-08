ea951a499949c23ff701a72ebda49b97cc0ed43d
강찬석 <ssimjunlke@naver.com>
2021-09-08 10:22:18 +0900

    Modules

---
+++ modules/test_module2.js
@@ -0,0 +1,15 @@
+
+const area = {
+    square: (length) => {
+        return length * length;
+    },
+    circle: radius => {
+        return Math.PI * radius ** 2;
+    },
+    rectangle: (width, height) => {
+        return width * height;
+    }
+}
+
+module.exports = area;
+
